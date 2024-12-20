433831fb0e79fb6d071520b61583d05bd3d3f0fd
AliyanA1 <124417710+AliyanA1@users.noreply.github.com>
2024-12-20 19:14:23 +0500

    Update dbconfig.js

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -2,12 +2,7 @@ const mongoose=require("mongoose");
 require('dotenv').config(); 
  
 mongoose.connect(process.env.MONGO_URL)
-.then(() => {
-    console.log("MongoDB is know connected");
-})
-.catch((error) => {
-    console.log(" there is Error in this code:", error);
-});
+
 
 module.exports=mongoose.connection;
 
